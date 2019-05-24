3c55e1add7b77986369914e4aa057e5895aff5c3
pukkamustard <pukkamustard@posteo.net>
2019-05-24 19:21:20 +0200

    get actor profile from database

--- main.rkt
+++ main.rkt
@@ -3,16 +3,98 @@
 (require web-server/servlet
          web-server/servlet-env
          web-server/dispatch
+         net/url
          json
          linkeddata/json-ld
          linkeddata/rdf
          datalog)
 
-(define knowledge (make-theory))
+;; Database
+;; ========
 
+;; The database is a datalog theory.
+(define database
+  (make-parameter (make-theory)))
+
+;; Add a triple to the database.
+;; TODO: this is my first syntax-rule. Read more of "Fear of Macros" (http://www.greghendershott.com/fear-of-macros/index.html) and make this nicer.
+(define-syntax-rule (database-add-triple! t)
+  (datalog (database) (! (triple
+                      (triple-subject t)
+                      (triple-predicate t)
+                      (triple-object t)))))
+
+;; TODO: just have a macro that adds multiple triples at the same time.
+(define (database-add-triples! ts)
+  (for ([t ts])
+    (database-add-triple! t)))
+
+;; Parse data to RDF triples
+(define (data->triples data)
+  (flatten
+   (hash-values
+    (json-ld->rdf (expand-jsonld (bytes->jsexpr data))))))
+
+;; get all triples with subject id and format as jsexpr
+(define (database-get-subject id)
+  (define (query->hash q)
+    (foldl (lambda (triple-as-hash h)
+             (let ([property (hash-ref triple-as-hash 'P)]
+                   [object (hash-ref triple-as-hash 'O)])
+               (hash-set h
+                         (string->symbol property)
+                         (cond [(literal? object) (literal-lexical-form object)]
+                               [else object]))))
+           (hasheq)
+           q))
+  (let [(query-result
+         (datalog (database) (? (triple id P O))))]
+    (hash-set
+     (query->hash query-result)
+     '@id id)))
+
+
+;; Debugging helpers
+;; =================
+
+;; Just an example triple
+(define a-triple
+  (triple "http://example.com/test#example1"
+          "http://example.com/vocab#embed"
+          "http://example.com/vocab#example2"))
+
+
+;; Web Server
+;; ==========
+
+;; Set up routes
+(define-values (dispatch url)
+  (dispatch-rules
+   ;; The actor profile
+   [("") get-actor-profile]
+
+   ;; inbox
+   [("inbox") get-inbox]
+
+   ;; TODO outbox
+   [("outbox") hello]
+
+   ;; any JSON-LD posted here will be added to the database
+   [("post") #:method "post" post!]
+
+   [else error-404]))
+
+(define base-url
+  (make-parameter (string->url "http://localhost:8000/")))
+
+(define (absolute-url handler)
+  (combine-url/relative (base-url) (url handler)))
+
+;; Respond with a plain text string
 (define (response/string
          #:code [code 200]
-         #:body [body ""])
+         #:body [body ""]
+         )
   (response/full
    code
    (string->bytes/utf-8 "Yo")
@@ -21,45 +103,82 @@
    empty
    (list (string->bytes/utf-8 body))))
 
-(define (index req)
-  (response/string #:body (format "~a" knowledge)))
+;; Respond with JSON-LD
+(define (response/jsonld
+         #:code [code 200]
+         #:body [body (hash)]
+         )
+  (response/full
+   code
+   (string->bytes/utf-8 "Yo")
+   (current-seconds)
+   ;; well, really with activity+json mime type
+   (string->bytes/utf-8 "application/activity+json")
+   empty
+   (list (jsexpr->bytes body))))
 
+(define (error-404 req)
+  (response/string #:code 404 #:body "Not found"))
+
+;; Get actor profile from DB and return
+;; TODO: compacting does not yet work as I would like it to.
+(define (get-actor-profile req)
+  (let [(actor-profile
+         (jsexpr->string
+          (compact-jsonld
+           (database-get-subject "http://localhost:8000/") as:context)))]
+    (response/jsonld #:body actor-profile)))
+
+;; TODO
+(define (get-inbox req)
+  (response/string #:body "Hello"))
+
+;; Hiii!
 (define (hello req name)
   (response/string
    #:body (string-append "Hello " name ".")))
 
-(define (data->rdf data)
-  (flatten
-   (hash-values
-    (json-ld->rdf (expand-jsonld (bytes->jsexpr data))))))
-
-(define a-triple
-  (triple "http://example.com/test#example1"
-          "http://example.com/vocab#embed"
-          "http://example.com/vocab#example2"))
-
-(define-syntax-rule (knowledge-add-triple! theory t)
-  (datalog theory (! (triple
-                      (triple-subject t)
-                      (triple-predicate t)
-                      (triple-object t)))))
-
+;; Just add some stuff to the DB
 (define (post! req)
-  (let* ([data (request-post-data/raw req)]
-         [rdf (data->rdf data)])
-    (for ([triple rdf])
-      (knowledge-add-triple! knowledge triple))
+  (let ([data (request-post-data/raw req)])
+    (database-add-triples! (data->triples data))
     (response/string #:body "Ok")))
 
-(define-values (go _)
-    (dispatch-rules
-     [("") index]
-     [("post") #:method "post" post!]
-     [ ("hello" (string-arg)) hello]
-     [else index]))
+
+;; ActivityPub
+;; ===========
+
+;; ActivityStreams context
+(define as:context
+  (hash-ref
+   (call-with-input-file "as.jsonld" read-json) '@context))
+
+;; An actor, the actor
+(define the-actor
+  (let ([id (url->string (base-url))]
+        [name "The Actor"])
+    (list
+     (triple id
+             "https://www.w3.org/ns/activitystream#inbox"
+             (url->string (absolute-url get-inbox)))
+     (triple id
+             rdf:type
+             "https://www.w3.org/ns/activitystream#Person")
+     (triple id
+             "https://www.w3.org/ns/activitystreams#name"
+             (literal name #f #f)))))
 
 
-(serve/servlet go
-               #:servlet-regexp #rx""
-               #:launch-browser? #f
-               )
+;; Do it!
+;; ======
+(define (main)
+  ;; Add the actor to the db
+  (database-add-triples! the-actor)
+
+  ;; Start web server
+  (serve/servlet dispatch
+                 #:servlet-regexp #rx""
+                 #:launch-browser? #f
+                 ))
+
+(main)
